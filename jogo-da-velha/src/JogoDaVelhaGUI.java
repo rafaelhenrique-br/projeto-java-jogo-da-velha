db94021547c999cb918495e96a41be85e2775baa
Rocktx <128630951+Rocktx@users.noreply.github.com>
2023-11-16 21:04:10 -0300

    Update JogoDaVelhaGUI.java
    Henrique Roque

--- jogo-da-velha/src/JogoDaVelhaGUI.java
+++ jogo-da-velha/src/JogoDaVelhaGUI.java
@@ -96,4 +96,23 @@ public JogoDaVelhaGUI() {
             }
         }
     }
+    private boolean verificarVitoria() {
+        // verificar a vitória
+        for (int i = 0; i < 3; i++) {
+            if (tabuleiro[i][0] == jogadorAtual && tabuleiro[i][1] == jogadorAtual && tabuleiro[i][2] == jogadorAtual) {
+                return true; // Linhas
+            }
+            if (tabuleiro[0][i] == jogadorAtual && tabuleiro[1][i] == jogadorAtual && tabuleiro[2][i] == jogadorAtual) {
+                return true; // Colunas
+            }
+        }
+        if (tabuleiro[0][0] == jogadorAtual && tabuleiro[1][1] == jogadorAtual && tabuleiro[2][2] == jogadorAtual) {
+            return true; // Diagonal principal
+        }
+        if (tabuleiro[0][2] == jogadorAtual && tabuleiro[1][1] == jogadorAtual && tabuleiro[2][0] == jogadorAtual) {
+            return true; // Diagonal secundária
+        }
+
+        return false; // não encontrada
+    }
 
